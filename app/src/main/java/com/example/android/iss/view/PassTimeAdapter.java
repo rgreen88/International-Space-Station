3c1b90ffbff57ef6403436d0f7583f6be39e6589
rgreen88 <rynelgreen@gmail.com>
2017-12-07 17:35:41 -0500

    creating adapter for recyclerview

--- app/src/main/java/com/example/android/iss/view/PassTimeAdapter.java
+++ app/src/main/java/com/example/android/iss/view/PassTimeAdapter.java
@@ -4,5 +4,9 @@ package com.example.android.iss.view;
  * Created by rynel on 12/7/2017.
  */
 
-public class RecyclerAdapter {
+public class PassTimeAdapter {
+
+    //creating private int mNumberOfTimesPassed
+    private int mNumberOfTimesPassed;
+
 }
