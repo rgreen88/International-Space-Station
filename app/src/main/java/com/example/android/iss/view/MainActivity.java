38bae984be63ed63ce1bd4cadeff83f0bfa1b484
rgreen88 <rynelgreen@gmail.com>
2017-12-07 17:53:11 -0500

    Generated RecyclerView layout and referencing PassTimeAdapter. May rename to TimesPassed

--- app/src/main/java/com/example/android/iss/view/MainActivity.java
+++ app/src/main/java/com/example/android/iss/view/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.android.iss.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.android.iss.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int NUM_ITEMS = 30;
+
+    //instantiating RecyclerView and Adapter
+    private PassTimeAdapter mAdapter;
+    private RecyclerView mNumberOfTimesPassed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //binding rv
+        mNumberOfTimesPassed = findViewById(R.id.rv_recyclerView);
+
+        //LinearLayoutManager for managing items in RecyclerView
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mNumberOfTimesPassed.setLayoutManager(layoutManager);
+        //mNumberOfTimesPassed.setHasFixedSize(true); <--check later
+
+
     }
 }
--- app/src/main/java/com/example/android/iss/view/PassTimeAdapter.java
+++ app/src/main/java/com/example/android/iss/view/PassTimeAdapter.java
@@ -1,5 +1,9 @@
 package com.example.android.iss.view;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by rynel on 12/7/2017.
  */
@@ -9,4 +13,18 @@ public class PassTimeAdapter {
     //creating private int mNumberOfTimesPassed
     private int mNumberOfTimesPassed;
 
+    class NumberOfPassesViewHolder extends RecyclerView.ViewHolder{
+
+        TextView listNumberOfPassesView;
+
+        public NumberOfPassesViewHolder(View itemView){
+            super(itemView);
+        }
+
+        public void bind (int listIndex){
+
+            listNumberOfPassesView.setText(String.valueOf(listIndex));
+
+        }
+    }
 }
