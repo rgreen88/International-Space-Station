3b03355cdf32604ee7a7fd62411eb147a292ef4a
rgreen88 <rynelgreen@gmail.com>
2017-12-07 20:11:00 -0500

    outlining details as to what to do for intended results after retrieving data

--- app/src/main/java/com/example/android/iss/viewmodel/DataRetrieval.java
+++ app/src/main/java/com/example/android/iss/viewmodel/DataRetrieval.java
@@ -5,4 +5,13 @@ package com.example.android.iss.viewmodel;
  */
 
 public class DataRetrieval {
+
+    //TODO: need to call these methods commented before and store in TextView for the coordinates
+    //TODO: and passes into the recyclerview
+
+    //getLatitude() coordinates on earth
+
+    //getLongitude() coordinates on earth
+
+    //getPasses() number of passes satellite makes
 }
